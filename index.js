b86c22857f2ce4a78ea93274959db6f813b05464
austinoso <osorio_austin@yahoo.com>
2020-03-09 14:37:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event_name) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event_name} gift!`);
+    };
+    return arr;
+};
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number);
+        number--;
+    };
+};
