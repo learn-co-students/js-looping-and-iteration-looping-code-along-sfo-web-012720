3d4dd2b29c7d21869a094bc4d0248bdac7dfcdd6
JABermudez11 <justinanbermudez@gmail.com>
2020-03-08 21:17:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(people, message){
+    let result = [];
+    for( let i = 0; i < people.length; i++ ){
+        result.push(`Thank you, ${people[i]}, for the wonderful ${message} gift!`);        
+    }
+    return result;
+}
+
+//writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(num) {
+    while(num > 0){
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
